db37145b88c8689ad8a64c377a8230c95633a7fe
Kevin OMara <kgomara@yahoo.com>
2013-12-28 13:28:08 -0600

    Initial Commit

---
+++ OCAEditableCollectionViewFlowLayout/OCAEditableCollectionViewFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  OCAEditableCollectionViewFlowLayout.h
+//  OCAEditableCollectionViewFlowLayout
+//
+//  Created by Kevin O'Mara on 12/28/13.
+//  Copyright (c) 2013 O'Mara Consulting Associates. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCAEditableCollectionViewFlowLayout : NSObject
+
+@end
---
+++ OCAEditableCollectionViewFlowLayout/OCAEditableCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  OCAEditableCollectionViewFlowLayout.m
+//  OCAEditableCollectionViewFlowLayout
+//
+//  Created by Kevin O'Mara on 12/28/13.
+//  Copyright (c) 2013 O'Mara Consulting Associates. All rights reserved.
+//
+
+#import "OCAEditableCollectionViewFlowLayout.h"
+
+@implementation OCAEditableCollectionViewFlowLayout
+
+@end
